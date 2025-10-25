c12689ac8240df86b3bbdf41e5d2ad8a1cf59f0f
Anirudh Aggarwal <ani.agg923@gmail.com>
2025-10-25 14:17:31 +0530

    squares

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,19 @@ import { useState } from 'react'
 import './App.css'
 import Landing from './components/Landing/Landing'
 import GooeyNav from './components/Navbar/Navbar'
+import Squares from './components/Squares/Squares'
 
 function App() {
   return (
     <div className="app">
       <GooeyNav />
+      <Squares
+            speed={0.3}
+            squareSize={50}
+            direction="diagonal"
+            borderColor="rgba(255, 255, 255, 0.09)"
+            hoverFillColor="rgba(204, 153, 255, 0.5)"
+          />
       <Landing />
     </div>
   )
